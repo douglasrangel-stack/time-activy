144f4a2289b6c0373e7716c8a5eaf541a7a759e6
Douglas Rangel <douglascoutinho202@gmail.com>
2023-08-25 11:22:03 -0300

    add project and layout page

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar/>
+    </div>
+    <div class="column is-three-quarter">
+      <FormFields/>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue' ;
+import FormFields from './components/FormFields.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SideBar,
+    FormFields
+  }
+});
+</script>
+
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.css';
+
+
+createApp(App).mount('#app')
