8c7502a2f5c0b3fc1a6cc4e369c14063ea3df7ac
Douglas Rangel <douglas.rangel@jazztech.com.br>
2023-08-31 14:59:12 -0300

    add vou router

---
+++ src/views/Activity.vue
@@ -0,0 +1,38 @@
+<template>
+    <FormFields @save="saveActivity"/>
+    <div class="space">
+        <div class="lista">
+        <NotFoundVue v-if="activities.length === 0" text="Nenhuma atividade cadastrada!" />
+        <ActivityRow v-for="(activity, index) in activities" :key="index" :activity="activity"/>
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import FormFields from '../components/FormFields.vue'
+  import ActivityRow from '../components/ActivityRow.vue';
+  import IActivity from '../interfaces/IActivity'
+  import NotFoundVue from '../components/NotFound.vue';
+  
+  export default defineComponent({
+    name: 'Activity',
+    components: {
+      FormFields,
+      ActivityRow,
+      NotFoundVue
+    },
+    data() {
+      return {
+        activities: [] as IActivity[]
+      }
+    },
+    methods: {
+      saveActivity( activity: IActivity) {
+        this.activities.push(activity);
+        this.activities.reverse();
+      }
+    }
+  });
+</script>
+  
